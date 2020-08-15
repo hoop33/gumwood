63c0538a20627e8d799d59ef625be990bbdad982
Rob Warner <rwarner@grailbox.com>
2020-08-15 15:28:28 -0400

    feat: add function for header

--- src/markdown.rs
+++ src/markdown.rs
@@ -10,3 +10,27 @@ impl Markdown {
         Ok(Markdown { front_matter })
     }
 }
+
+fn header(level: u8, text: &str) -> String {
+    // Note that we don't bounds check level -- it's a private function, after all
+    format!(
+        "{} {}\n\n",
+        (0..level).map(|_| "#").collect::<String>(),
+        text
+    )
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_header_should_create_header_1() {
+        assert_eq!("# My Header\n\n", header(1, "My Header"));
+    }
+
+    #[test]
+    fn test_header_should_create_header_6() {
+        assert_eq!("###### My Header\n\n", header(6, "My Header"));
+    }
+}
